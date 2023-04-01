65123973a441ef1c93a910bcc24bb4b768e75b81
Ashish Gupta <ashish000189@gmail.com>
2023-04-02 01:51:35 +0530

    worked on working of buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export class NavBar extends Component {
     
     return (
         <div>
-        <nav className="navbar navbar-expand-lg bg-body-dark">
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">NewsMonk</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,11 +21,11 @@ export class NavBar extends Component {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <a className="nav-link" href="/">About</a>
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+                 Know More 
                 </a>
                 <ul className="dropdown-menu">
                   <li><a className="dropdown-item" href="/">Action</a></li>
@@ -34,9 +34,7 @@ export class NavBar extends Component {
                   <li><a className="dropdown-item" href="/">Something else here</a></li>
                 </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
+              
             </ul>
            
           </div>
--- src/components/News.js
+++ src/components/News.js
@@ -8,22 +8,53 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page:1
     };
   }
 
+
+
+
  async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=2dfd3bfcb9564c2b8d03157705cd94ce";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2dfd3bfcb9564c2b8d03157705cd94ce&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles , totalResults : parsedData.totalResults})
   }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2dfd3bfcb9564c2b8d03157705cd94ce&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+        page: this.state.page -1,
+        articles: parsedData.articles
+    })
+    }
+    
+    handleNextClick = async () => {
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+    else{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2dfd3bfcb9564c2b8d03157705cd94ce&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+    this.setState({
+        page: this.state.page +1,
+        articles: parsedData.articles})
+    }
+    }
   render() {
     return (
-      <div className="container my-3">
-        <h1>NewsMonk - Top Headlines</h1>
+      <div className="container my-3 bg-white">
+        <h1 className="text-center text-white">NewsMonk - Top Headlines</h1>
 
-        <div className="row my-3">
+
+        <div className="row my-10">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem
@@ -34,6 +65,12 @@ export class News extends Component {
               />
             </div>;
           })}
+        </div>
+        <div className="container">
+        <div class="d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button></div>
+        
         </div>
       </div>
     );
