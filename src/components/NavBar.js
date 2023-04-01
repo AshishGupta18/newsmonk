d2e5f2d2608a420d80204fd32cf6e28d4322af09
Ashish Gupta <ashish000189@gmail.com>
2023-04-01 23:47:54 +0530

    started using apikey

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export class NavBar extends Component {
     
     return (
         <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">NewsMonk</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="/"/>
+  <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/03/31/1600x900/Congress-leader-Rahul-Gandhi--File-Photo-_1680265816412.jpg":imageUrl} className="card-img-top" alt="/"/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
 
